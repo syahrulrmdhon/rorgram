b0207d1016183ba159a75d4c2f77d475e0709e07
Syahrul Romadhon <syahrulrmdhon2@gmail.com>
2018-08-16 23:53:45 +0700

    layoting profile

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'profile/profile'
+
   get 'home/index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
